8e24526a7b65f5a797792fc94388c3dce04267d4
rrcabrera9625 <rrcabrera9625@protonmail.com>
2021-12-02 11:44:54 -0500

    Actualizado Repositorio Perfil JPA #2

--- teatro/src/main/java/com/curso/springboot/teatro/repository/PerfilRepository.java
+++ teatro/src/main/java/com/curso/springboot/teatro/repository/PerfilRepository.java
@@ -1,9 +1,13 @@
 package com.curso.springboot.teatro.repository;
 
+import java.util.Optional;
+
 import com.curso.springboot.teatro.model.Perfil;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PerfilRepository extends JpaRepository<Perfil, Integer> {
+    Optional<Perfil> findByPerfilNombre(String perfilNombre);
+
     
 }
