098fd26da39b49612a7b7f620b195ed8d69916d4
rrcabrera9625 <rrcabrera9625@protonmail.com>
2021-12-02 15:10:39 -0500

    Modelos de Datos modificados #1

--- teatro/src/main/java/com/curso/springboot/teatro/model/Butaca.java
+++ teatro/src/main/java/com/curso/springboot/teatro/model/Butaca.java
@@ -1,15 +1,12 @@
 package com.curso.springboot.teatro.model;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,22 +14,44 @@ import lombok.Setter;
 @Getter @Setter
 @Table(name="butaca")
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="tipo_butaca", 
-  discriminatorType = DiscriminatorType.INTEGER)
 public class Butaca {
 
     @Id @Column(name = "BUTACA_ID")
 	private int id;
 
+    @Column(name = "tipo_butaca")
+    private int tipo_butaca;
+
     @Column(name = "vendida")
 	private Boolean vendida;
+    
+    @Column(name = "areafumadores", nullable = true)
+	private Boolean areafumadores;
+
+    @Column(name = "numerobalcon", nullable = true)
+	private int numerobalcon;
+
+    @Column(name = "protocolo", nullable = true)
+	private Boolean protocolo;
 
     @ManyToOne
     @JoinColumn(name="TEATRO_ID", nullable=true)
     private Teatro teatro;
 
     public Butaca() {
+    }
+
+    public Double precio(){
+        if (this.tipo_butaca == 1){
+            return (this.protocolo)? 30.00 : 20.00;
+        }
+        else {
+            return (this.areafumadores)? this.numerobalcon * 0.5 + 5.00 : this.numerobalcon * 0.5 ;
+        }   
+    }
+
+    public void vender(){
+        this.vendida = true;
     }
 
 }
