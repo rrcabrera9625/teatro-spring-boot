058b29c5aad379f287032479ee850fd769be84d4
rrcabrera9625 <rrcabrera9625@protonmail.com>
2021-12-02 11:29:23 -0500

    Actualizado Repositorio Usuario JPA #2

--- teatro/src/main/java/com/curso/springboot/teatro/repository/UsuarioRepository.java
+++ teatro/src/main/java/com/curso/springboot/teatro/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.curso.springboot.teatro.repository;
 
+import java.util.Optional;
+
 import com.curso.springboot.teatro.model.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsuario(String usuario);
     
 }
