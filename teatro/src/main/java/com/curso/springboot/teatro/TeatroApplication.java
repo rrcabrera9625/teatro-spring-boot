d33d098f0ee6c43ab89fe498047ed0d652d310d3
rrcabrera9625 <rrcabrera9625@protonmail.com>
2021-12-02 09:25:53 -0500

    Creando estructura del proyecto

---
+++ teatro/src/main/java/com/curso/springboot/teatro/TeatroApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.teatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeatroApplication.class, args);
+	}
+
+}
---
+++ teatro/src/test/java/com/curso/springboot/teatro/TeatroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.teatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
