8d0d3b4039d530fbcb650ef48f1bbb933817573a
rrcabrera9625 <rrcabrera9625@protonmail.com>
2021-12-06 08:25:32 -0500

    Crear Repositorios JPA #2

--- teatro/src/main/java/com/curso/springboot/teatro/repository/PerfilRepository.java
+++ teatro/src/main/java/com/curso/springboot/teatro/repository/PerfilRepository.java
@@ -8,6 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PerfilRepository extends JpaRepository<Perfil, Integer> {
     Optional<Perfil> findByPerfilNombre(String perfilNombre);
-
-    
+    Boolean existsByPerfilNombre(String perfilNombre);
 }
